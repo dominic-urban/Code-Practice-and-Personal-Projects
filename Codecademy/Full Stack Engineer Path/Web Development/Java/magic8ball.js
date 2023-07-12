6310688fc68fda9cc9b1506fa5e8795e3e6b3494
dominic-urban <125234256+dominic-urban@users.noreply.github.com>
2023-07-12 15:26:57 +1000

    Create magic8ball.js
    little js version of magic 8 ball program - using a switch

---
+++ Codecademy/Full Stack Engineer Path/Web Development/Java/magic8ball.js
@@ -0,0 +1,40 @@
+let userName = 'Test User';
+
+userName ? console.log(`Hello ${userName}!`) : console.log('Hello!');
+
+let userQuestion = 'Should I ask the Magic 8 Ball a Question?';
+console.log(`${userName} asked: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eigthBall = '';
+
+switch (randomNumber) {
+  case 0:
+    console.log('It is certain');
+    break;
+  case 1:
+    console.log('It is decidedly so');
+    break;
+  case 2:
+    console.log('Reply hazy try again');
+    break;
+  case 3:
+    console.log('Cannot predict now');
+    break;
+  case 4:
+    console.log('Do not count on it');
+    break;
+  case 5:
+    console.log('My sources say no');
+    break;
+  case 6:
+    console.log('Outlook not so good');
+    break;
+  case 7:
+    console.log('Signs point to yes');
+    break;
+  default:
+    console.log('Something went wrong.');
+    break;
+}
